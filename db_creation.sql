ff37f03d95382001cadc8ae2e29824a398847354
mhotting <madric.hottinger.101@gmail.com>
2019-04-02 14:55:22 +0200

    Corrected db_creation

--- db_creation.sql
+++ db_creation.sql
@@ -18,142 +18,142 @@ USE `db_matcha`;
 
 -- t_user
 CREATE TABLE `db_matcha`.`t_user` (
-  `usr_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `usr_uname` VARCHAR(255) NOT NULL,
-  `usr_fname` VARCHAR(255) NOT NULL,
-  `usr_lname` VARCHAR(255) NOT NULL,
-  `usr_birthDate` DATE NOT NULL,
-  `usr_email` VARCHAR(255) NOT NULL,
-  `usr_pwd` VARCHAR(255) NOT NULL,
-  `usr_age` INT NOT NULL,
-  `usr_gender` VARCHAR(1) NOT NULL,
-  `usr_bio` TEXT NULL,
-  `usr_score` INT NOT NULL DEFAULT 0,
-  `usr_status` VARCHAR(255) NULL,
-  `usr_location` VARCHAR(255) NULL,
-  `usr_creationDate` DATETIME NOT NULL DEFAULT NOW(),
-  `usr_connectionDate` DATETIME NULL,
-  `usr_activationToken` VARCHAR(255) NOT NULL,
-  `usr_pwdToken` VARCHAR(255) NOT NULL,
-  `usr_idOrientation` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`usr_id`),
-  UNIQUE INDEX `usr_id_UNIQUE` (`usr_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `usr_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `usr_uname` VARCHAR(255) NOT NULL,
+    `usr_fname` VARCHAR(255) NOT NULL,
+    `usr_lname` VARCHAR(255) NOT NULL,
+    `usr_birthDate` DATE,
+    `usr_email` VARCHAR(255) NOT NULL,
+    `usr_pwd` VARCHAR(255) NOT NULL,
+    `usr_age` INT,
+    `usr_gender` VARCHAR(1),
+    `usr_bio` TEXT NULL,
+    `usr_score` INT DEFAULT 0,
+    `usr_status` VARCHAR(255),
+    `usr_location` VARCHAR(255),
+    `usr_creationDate` DATETIME DEFAULT NOW(),
+    `usr_connectionDate` DATETIME,
+    `usr_activationToken` VARCHAR(255),
+    `usr_pwdToken` VARCHAR(255),
+    `usr_idOrientation` INT UNSIGNED,
+    PRIMARY KEY (`usr_id`),
+    UNIQUE INDEX `usr_id_UNIQUE` (`usr_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_message
 CREATE TABLE `db_matcha`.`t_message` (
-  `msg_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `msg_content` TEXT NULL,
-  `msg_creationDate` DATETIME NOT NULL DEFAULT NOW(),
-  `msg_idSender` INT UNSIGNED NOT NULL,
-  `msg_idReceiver` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`msg_id`),
-  UNIQUE INDEX `msg_id_UNIQUE` (`msg_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `msg_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `msg_content` TEXT NULL,
+    `msg_creationDate` DATETIME NOT NULL DEFAULT NOW(),
+    `msg_idSender` INT UNSIGNED NOT NULL,
+    `msg_idReceiver` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`msg_id`),
+    UNIQUE INDEX `msg_id_UNIQUE` (`msg_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_image
 CREATE TABLE `db_matcha`.`t_image` (
-  `image_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `image_path` VARCHAR(255) NOT NULL,
-  `image_idUser` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`image_id`),
-  UNIQUE INDEX `image_id_UNIQUE` (`image_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `image_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `image_path` VARCHAR(255) NOT NULL,
+    `image_idUser` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`image_id`),
+    UNIQUE INDEX `image_id_UNIQUE` (`image_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_orientation
 CREATE TABLE `db_matcha`.`t_orientation` (
-  `orientation_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `orientation_name` VARCHAR(45) NOT NULL,
-  PRIMARY KEY (`orientation_id`),
-  UNIQUE INDEX `orientation_id_UNIQUE` (`orientation_id` ASC),
-  UNIQUE INDEX `orientation_name_UNIQUE` (`orientation_name` ASC)
-  )
-  ENGINE=InnoDB;
+    `orientation_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `orientation_name` VARCHAR(45) NOT NULL,
+    PRIMARY KEY (`orientation_id`),
+    UNIQUE INDEX `orientation_id_UNIQUE` (`orientation_id` ASC),
+    UNIQUE INDEX `orientation_name_UNIQUE` (`orientation_name` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_interest
 CREATE TABLE `db_matcha`.`t_interest` (
-  `interest_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `interest_name` VARCHAR(255) NOT NULL,
-  PRIMARY KEY (`interest_id`),
-  UNIQUE INDEX `interest_id_UNIQUE` (`interest_id` ASC),
-  UNIQUE INDEX `interest_name_UNIQUE` (`interest_name` ASC)
-  )
-  ENGINE=InnoDB;
+    `interest_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `interest_name` VARCHAR(255) NOT NULL,
+    PRIMARY KEY (`interest_id`),
+    UNIQUE INDEX `interest_id_UNIQUE` (`interest_id` ASC),
+    UNIQUE INDEX `interest_name_UNIQUE` (`interest_name` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_notif
 CREATE TABLE `db_matcha`.`t_notification` (
-  `notif_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `notif_creationDate` DATETIME NOT NULL DEFAULT NOW(),
-  `notif_consulted` TINYINT NOT NULL DEFAULT 0,
-  `notif_category` VARCHAR(45) NOT NULL,
-  `notif_idUser` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`notif_id`),
-  UNIQUE INDEX `notif_id_UNIQUE` (`notif_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `notif_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `notif_creationDate` DATETIME NOT NULL DEFAULT NOW(),
+    `notif_consulted` TINYINT NOT NULL DEFAULT 0,
+    `notif_category` VARCHAR(45) NOT NULL,
+    `notif_idUser` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`notif_id`),
+    UNIQUE INDEX `notif_id_UNIQUE` (`notif_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_userInterest
 CREATE TABLE `db_matcha`.`t_userInterest` (
-  `userInterest_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `userInterest_idUser` INT UNSIGNED NOT NULL,
-  `userInterest_idInterest` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`userInterest_id`),
-  UNIQUE INDEX `userInterest_id_UNIQUE` (`userInterest_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `userInterest_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `userInterest_idUser` INT UNSIGNED NOT NULL,
+    `userInterest_idInterest` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`userInterest_id`),
+    UNIQUE INDEX `userInterest_id_UNIQUE` (`userInterest_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_visit
 CREATE TABLE `db_matcha`.`t_visit` (
-  `visit_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `visit_idVisitor` INT UNSIGNED NOT NULL,
-  `visit_idVisited` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`visit_id`),
-  UNIQUE INDEX `visit_id_UNIQUE` (`visit_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `visit_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `visit_idVisitor` INT UNSIGNED NOT NULL,
+    `visit_idVisited` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`visit_id`),
+    UNIQUE INDEX `visit_id_UNIQUE` (`visit_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_dislike
 CREATE TABLE `db_matcha`.`t_dislike` (
-  `dislike_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `dislike_idDisliked` INT UNSIGNED NOT NULL,
-  `dislike_idDisliker` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`dislike_id`),
-  UNIQUE INDEX `dislike_id_UNIQUE` (`dislike_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `dislike_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `dislike_idDisliked` INT UNSIGNED NOT NULL,
+    `dislike_idDisliker` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`dislike_id`),
+    UNIQUE INDEX `dislike_id_UNIQUE` (`dislike_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_like
 CREATE TABLE `db_matcha`.`t_like` (
-  `like_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `like_idLiked` INT UNSIGNED NOT NULL,
-  `like_idLiker` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`like_id`),
-  UNIQUE INDEX `like_id_UNIQUE` (`like_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `like_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `like_idLiked` INT UNSIGNED NOT NULL,
+    `like_idLiker` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`like_id`),
+    UNIQUE INDEX `like_id_UNIQUE` (`like_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_block
 CREATE TABLE `db_matcha`.`t_block` (
-  `block_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `block_idBlocked` INT UNSIGNED NOT NULL,
-  `block_idBlocker` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`block_id`),
-  UNIQUE INDEX `block_id_UNIQUE` (`block_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `block_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `block_idBlocked` INT UNSIGNED NOT NULL,
+    `block_idBlocker` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`block_id`),
+    UNIQUE INDEX `block_id_UNIQUE` (`block_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 -- t_signal
 CREATE TABLE `db_matcha`.`t_signal` (
-  `signal_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `signal_idSignaled` INT UNSIGNED NOT NULL,
-  `signal_idSignalor` INT UNSIGNED NOT NULL,
-  PRIMARY KEY (`signal_id`),
-  UNIQUE INDEX `signal_id_UNIQUE` (`signal_id` ASC)
-  )
-  ENGINE=InnoDB;
+    `signal_id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `signal_idSignaled` INT UNSIGNED NOT NULL,
+    `signal_idSignalor` INT UNSIGNED NOT NULL,
+    PRIMARY KEY (`signal_id`),
+    UNIQUE INDEX `signal_id_UNIQUE` (`signal_id` ASC)
+    )
+    ENGINE=InnoDB;
 
 
 /* ************************************************************ */
