761fa789675a7e132c71771d8692038b3d5c173d
Kevin  Rambonona <krambono@student.le-101.fr>
2019-04-02 10:44:59 +0200

    script manage

---
+++ DATABASE_DOCKER/manage.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+usage='usage: ./manage.sh [ start | stop | log ]'
+
+if [ $# -ne 1 ]
+then
+	echo $usage
+else
+	if [ $1 = 'start' ]
+	then
+		docker-compose up >> log.txt 2>&1 &
+	elif [ $1 = 'stop' ]
+	then
+		docker-compose down
+	elif [ $1 = 'log' ]
+	then
+		if [ -f 'log.txt' ]
+		then
+			tail log.txt
+		fi
+	else
+		echo $usage;
+	fi
+fi
