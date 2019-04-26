c297be405d056ab8ccc72e34209dfa51af7e9b69
motting <mhotting@student.le-101.fr>
2019-04-26 11:57:34 +0200

    Fake interests generated

--- APP/database/hydrateDb.js
+++ APP/database/hydrateDb.js
@@ -5,81 +5,194 @@
  *  2019
 ********************************************************************************/
 
-function cleanUsername(username) {
-    const regex = /^[a-zA-Z0-9]$/;
-    let newUsername = '';
-    for (let char of username){
-        if (regex.test(char))
-            newUsername += char;
-    }
-    return newUsername; 
-}
-
-
 const db = require('./../util/database');
 const bcrypt = require('bcrypt');
 const faker = require('faker');
 const crypto = require('crypto');
 faker.locale = 'fr';
 
+/****************************************/
+/*  USEFUL FUNCTIONS                    */
+/****************************************/
+// Clean an username by removing none alphanumeric chars
+const cleanUsername = (username) => {
+    const regex = /^[a-zA-Z0-9]$/;
+    let newUsername = '';
+    for (let char of username) {
+        if (regex.test(char))
+            newUsername += char;
+    }
+    return newUsername;
+}
+
+// Remove french accents from a string
+const removeAccent = string => {
+    const accent = [
+        /[\300-\306]/g, /[\340-\346]/g, // A, a
+        /[\310-\313]/g, /[\350-\353]/g, // E, e
+        /[\314-\317]/g, /[\354-\357]/g, // I, i
+        /[\322-\330]/g, /[\362-\370]/g, // O, o
+        /[\331-\334]/g, /[\371-\374]/g, // U, u
+        /[\321]/g, /[\361]/g, // N, n
+        /[\307]/g, /[\347]/g, // C, c
+    ];
+    const noaccent = ['A', 'a', 'E', 'e', 'I', 'i', 'O', 'o', 'U', 'u', 'N', 'n', 'C', 'c'];
+
+    for (let i = 0; i < accent.length; i++) {
+        string = string.replace(accent[i], noaccent[i]);
+    }
+    return string;
+}
+
+// Check if a given char is alphanumeric
+const isLetter = c => {
+    const regex = /^[a-zA-Z0-9]$/;
+    return regex.test(c);
+}
+
+// Clean a string by removing the accents, trimming it, etc.
+const cleanString = string => {
+    const array = [];
+    let newString = removeAccent(string);
+    for (let char of newString) {
+        if (isLetter(char))
+            array.push(char);
+    }
+    newString = array.join('');
+    return newString;
+}
+
+// Create user interests
+const createInterests = () => {
+    let interests = [];
+    let interest;
+    let promiseArray = [];
+    let i = 0;
+
+    while (i < 40) {
+        interest = cleanUsername(cleanString(faker.random.word()));
+        interest = interest.toLocaleLowerCase();
+        if (interest.length >= 3 && (interests.indexOf(interest) === -1)) {
+            interests.push(interest);
+            i++;
+        }
+    }
+    for (interest of interests) {
+        promiseArray.push(db.execute(
+            'INSERT INTO `db_matcha`.`t_interest` (`interest_name`) ' +
+            'VALUES (?);',
+            [interest]
+        ));
+    }
+    return (promiseArray);
+}
+
+/****************************************/
+/*  FAKE PROFILE GENERATOR              */
+/****************************************/
 const userTab = [];
 const promiseArray = [];
+const unameArray = [];
+let interestPromiseArray = [];
 const userObj = {};
 const password = bcrypt.hashSync('Qwerty123', 12);
 let rdmNbr;
+let i;
 
-for (let i = 0; i < 500; i++) {
-    userObj.usr_uname = cleanUsername(faker.internet.userName());
-    userObj.usr_fname = faker.name.firstName();
-    userObj.usr_lname = faker.name.lastName();
-    userObj.usr_email = faker.internet.email();
-    userObj.usr_pwd = password;
-    age = 0;
-    while (age < 18 || age > 65) {
-        age = faker.random.number();
-    }
-    userObj.usr_age = age;
-    rdmNbr = Math.random();
-    userObj.usr_gender = (rdmNbr < 0.5 ? 'Male' : 'Female');
-    userObj.usr_bio = faker.lorem.paragraph();
-    userObj.usr_status = "OK";
+interestPromiseArray = createInterests();
+console.log('Generating fake interests...');
+Promise.all(interestPromiseArray)
+    .then(() => {
+        console.log('OK');
+        console.log('Generating fake users...');
+        for (let i = 0; i < 500; i++) {
+            do {
+                userObj.usr_uname = cleanUsername(faker.internet.userName());
+            } while (unameArray.indexOf(userObj.usr_uname) !== -1);
+            unameArray.push(userObj.usr_uname);
+            userObj.usr_fname = faker.name.firstName();
+            userObj.usr_lname = faker.name.lastName();
+            userObj.usr_email = faker.internet.email();
+            userObj.usr_pwd = password;
+            age = 0;
+            while (age < 18 || age > 65) {
+                age = faker.random.number();
+            }
+            userObj.usr_age = age;
+            rdmNbr = Math.random();
+            userObj.usr_gender = (rdmNbr < 0.5 ? 'Male' : 'Female');
+            userObj.usr_bio = faker.lorem.paragraph();
+            userObj.usr_status = "OK";
 
-    // Longitude and latitude
-    do {
-        userObj.usr_longitude = faker.address.longitude();
-    } while (userObj.usr_longitude < -4.4744 || userObj.usr_longitude > 8.1350);
-    do {
-        userObj.usr_latitude = faker.address.latitude();
-    } while (userObj.usr_latitude < 42.1958 || userObj.usr_latitude > 51.0521);
+            // Longitude and latitude
+            do {
+                userObj.usr_longitude = faker.address.longitude();
+            } while (userObj.usr_longitude < -4.4744 || userObj.usr_longitude > 8.1350);
+            do {
+                userObj.usr_latitude = faker.address.latitude();
+            } while (userObj.usr_latitude < 42.1958 || userObj.usr_latitude > 51.0521);
 
-    // Tokens
-    userObj.usr_activationToken = crypto.createHash('md5').update(Math.random().toString()).digest('hex');
-    userObj.usr_pwdToken = crypto.createHash('md5').update(Math.random().toString()).digest('hex');
-    rdmNbr = Math.random();
+            // Tokens
+            userObj.usr_activationToken = crypto.createHash('md5').update(Math.random().toString()).digest('hex');
+            userObj.usr_pwdToken = crypto.createHash('md5').update(Math.random().toString()).digest('hex');
+            rdmNbr = Math.random();
 
-    // Score, gender and orientation
-    userObj.usr_score = Math.round(Math.random() * 100);
-    userObj.usr_orientation = (rdmNbr < 0.66 ? (rdmNbr < 0.5 ? 'bi' : 'homo')  : 'hetero');
-    userObj.usr_avatar = faker.internet.avatar();
+            // Score, gender and orientation
+            userObj.usr_score = Math.round(Math.random() * 100);
+            userObj.usr_orientation = (rdmNbr < 0.66 ? (rdmNbr < 0.5 ? 'bi' : 'homo') : 'hetero');
+            userObj.usr_avatar = faker.internet.avatar();
 
-    userTab.push({...userObj});
-}
+            userTab.push({ ...userObj });
+        }
 
-for (let user of userTab) {
-    promiseArray.push(db.execute(
-        'INSERT INTO `db_matcha`.`t_user` (`usr_uname`, `usr_fname`, `usr_lname`, `usr_email`, `usr_pwd`, `usr_age`, `usr_gender`, `usr_bio`, `usr_activationToken`, `usr_pwdToken`, `usr_orientation`, `usr_longitude`, `usr_latitude`, `usr_score`) ' +
-        'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
-        [
-            user.usr_uname, user.usr_fname, user.usr_lname,
-            user.usr_email, user.usr_pwd, user.usr_age,
-            user.usr_gender, user.usr_bio, user.usr_activationToken,
-            user.usr_pwdToken, user.usr_orientation,
-            user.usr_longitude, user.usr_latitude, user.usr_score
-        ]
-    ));
-}
+        for (let user of userTab) {
+            promiseArray.push(db.execute(
+                'INSERT INTO `db_matcha`.`t_user` (`usr_uname`, `usr_fname`, `usr_lname`, `usr_email`, `usr_pwd`, `usr_age`, `usr_gender`, `usr_bio`, `usr_activationToken`, `usr_pwdToken`, `usr_orientation`, `usr_longitude`, `usr_latitude`, `usr_score`) ' +
+                'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);',
+                [
+                    user.usr_uname, user.usr_fname, user.usr_lname,
+                    user.usr_email, user.usr_pwd, user.usr_age,
+                    user.usr_gender, user.usr_bio, user.usr_activationToken,
+                    user.usr_pwdToken, user.usr_orientation,
+                    user.usr_longitude, user.usr_latitude, user.usr_score
+                ]
+            ));
+        }
 
-Promise.all(promiseArray)
+        return (Promise.all(promiseArray));
+    })
+    .then(result => {
+        console.log('OK');
+        console.log('Generating fake userInterests...');
+        let tempNum;
+        const userInterestPromiseArray = [];
+        let num;
+        let nb_interests;
+
+        // Adding interests for each user
+        for (let i = 1; i <= 500; i++) {
+            tempNum = [];
+            // Generating a random number of interest
+            nb_interests = Math.round(Math.random() * 3);
+            for (let j = 0; j < nb_interests; j++) {
+                // Generating random interests IDs
+                do {
+                    num = Math.round(Math.random() * 39) + 1;
+                } while (tempNum.indexOf(num) !== -1)
+                tempNum.push(num);
+            }
+
+            // Adding userInterests in the DB
+            for (let idInterest of tempNum) {
+                userInterestPromiseArray.push(db.execute(
+                    'INSERT INTO `db_matcha`.`t_userInterest` (`userInterest_idUser`, `userInterest_idInterest`) ' +
+                    'VALUES (?, ?);',
+                    [i, idInterest]
+                ));
+            }
+        }
+        return (Promise.all(userInterestPromiseArray));
+    })
     .then(result => {
         console.log('SUCCESS - Fake data have been generated.');
     })
