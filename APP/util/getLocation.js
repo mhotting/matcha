f34f6dec29a3b0f4a4b5c5e7f709749be706ab09
Kevin  Rambonona <krambono@student.le-101.fr>
2019-05-04 14:41:40 +0200

    location

--- APP/util/getLocation.js
+++ APP/util/getLocation.js
@@ -1,10 +1,20 @@
 const axios = require('axios');
 
 module.exports = (latitude, longitude) => {
+    if (!latitude || !longitude)
+        return '';
     return axios.get('https://api.opencagedata.com/geocode/v1/json?q=' + latitude + '+' + longitude + '&key=aafdab68af81466d9fcc0b6f307e59f4')
         .then(data => {
             try {
-                return data.data.results.city;
+                const precisions = ['city', 'county', 'country'];
+                let location = '';
+                for (precision of precisions) {
+                    if (!!data.data.results[0].components[precision]){
+                        location = data.data.results[0].components[precision];
+                        break;
+                    }
+                }
+                return location;
             }
             catch (err) {
                 return '';
