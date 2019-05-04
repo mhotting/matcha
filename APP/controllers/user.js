75f31c1d279f8f016a6fca93fb9376a12f3054f2
motting <mhotting@student.le-101.fr>
2019-05-04 11:32:26 +0200

    Added images to getOtherInfos - cleaning a little bit

--- APP/controllers/user.js
+++ APP/controllers/user.js
@@ -38,7 +38,7 @@ exports.getInfos = (req, res, next) => {
             for (let image of userImages) {
                 imagesArray.push('http://localhost:8080/images/' + image.image_path);
             }
-            userInfos.images = imagesArray;
+            userInfos.photos = imagesArray;
             return (userInfos);
         })
         .then(userInfos => {
@@ -89,7 +89,7 @@ exports.getInfosCompatible = (req, res, next) => {
                         })
                         .then(likeStatus => {
                             pointB = { longitude: Number(Math.round(row.usr_longitude + 'e4') + 'e-4'), latitude: Number(Math.round(row.usr_latitude + 'e4') + 'e-4') };
-                            const distance = evalDistance({ ...pointA }, pointB); 
+                            const distance = evalDistance({ ...pointA }, pointB);
                             tempUser = {
                                 id: row.usr_id,
                                 uname: row.usr_uname,
@@ -99,7 +99,7 @@ exports.getInfosCompatible = (req, res, next) => {
                                 score: row.usr_score,
                                 distance: distance ? Math.round(distance * 100) / 100 : '',
                                 connection: row.date,
-                                images: imagesSave
+                                photos: imagesSave
                             };
                             tempUser.interests = interestsSave.map(interest => interest.interest_name);
 
@@ -118,7 +118,7 @@ exports.getInfosCompatible = (req, res, next) => {
         .catch(err => next(err));
 };
 
-// Get all the infos of the compatible users
+// Get all the infos of the matching users
 exports.getInfosMatch = (req, res, next) => {
     let matchingArray = [];
     let promiseArray = [];
@@ -160,7 +160,7 @@ exports.getInfosMatch = (req, res, next) => {
                         })
                         .then(likeStatus => {
                             pointB = { longitude: Number(Math.round(row.usr_longitude + 'e4') + 'e-4'), latitude: Number(Math.round(row.usr_latitude + 'e4') + 'e-4') };
-                            const distance = evalDistance({ ...pointA }, pointB); 
+                            const distance = evalDistance({ ...pointA }, pointB);
                             tempUser = {
                                 id: row.usr_id,
                                 uname: row.usr_uname,
@@ -170,7 +170,7 @@ exports.getInfosMatch = (req, res, next) => {
                                 score: row.usr_score,
                                 distance: distance ? Math.round(distance * 100) / 100 : '',
                                 connection: row.date,
-                                images: imagesSave
+                                photos: imagesSave
                                 // disabled: row.usr_status
                             };
                             if (
@@ -227,13 +227,6 @@ exports.getOtherInfo = (req, res, next) => {
                 connection: '02/05/19 14:32',
                 reported: true,
                 didLikeMe: true,
-                photos: [
-                    'https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60',
-                    'https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60',
-                    'https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250&q=80',
-                    'https://images.unsplash.com/photo-1518732714860-b62714ce0c59?auto=format&fit=crop&w=400&h=250&q=60',
-                    'https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60',
-                ],
                 // gender: user.usr_gender,
                 //orientation: user.usr_orientation
             };
@@ -244,7 +237,7 @@ exports.getOtherInfo = (req, res, next) => {
             return (Like.findById(req.userId, userInfos.id));
         })
         .then(like => {
-            userInfos.like = (like ?  'liked' : '');
+            userInfos.like = (like ? 'liked' : '');
             return (Visit.countVisit(userInfos.id));
         })
         .then(visit => {
@@ -253,7 +246,15 @@ exports.getOtherInfo = (req, res, next) => {
         })
         .then(block => {
             userInfos.blocked = (block ? true : false);
-            return userInfos;
+            return (Images.getAll(userInfos.id));
+        })
+        .then(images => {
+            let imagesArray = [];
+            for (let image of images) {
+                imagesArray.push('http://localhost:8080/images/' + image.image_path);
+            }
+            userInfos.photos = imagesArray;
+            return (userInfos);
         })
         .then(userInfos => {
             res.status(200).json({
@@ -261,10 +262,10 @@ exports.getOtherInfo = (req, res, next) => {
             });
         })
         .catch(err => next(err));
-        // la derniere date de connection
-        // les photos
-        // la distance et la localisation
-        // si il a ete signale par l'utilsateur connecte
-        // si cet user a like l'utiliatseur connecte
-        // Au boulot flemmard !!!
+    // la derniere date de connection
+    // les photos
+    // la distance et la localisation
+    // si il a ete signale par l'utilsateur connecte
+    // si cet user a like l'utiliatseur connecte
+    // Au boulot flemmard !!!
 };
